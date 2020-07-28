5c4d50bd36b58abb0237cf5752b2d12644fcf376
Terrence  Malone <terrenceam22@Terrences-MBP.hsd1.ga.comcast.net>
2020-07-28 13:31:12 -0400

    pulling categories with tekore

--- APP/app.py
+++ APP/app.py
@@ -3,6 +3,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import os
 from dotenv import load_dotenv
 from pprint import pprint
+import json
+import time
+import tekore as tk
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
@@ -10,7 +13,9 @@ client_secret_id = os.getenv("CLIENT_SECRET_ID")
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret_id)
 
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-search_str = 'Drake'
-result = sp.search(search_str, limit=1)
-pprint(result)
+app_token = tk.request_client_token(client_id, client_secret_id)
+spotify = tk.Spotify(app_token)
+
+all_cats = spotify.categories(limit=50)
+for cat in all_cats.items:
+    print(cat.name)

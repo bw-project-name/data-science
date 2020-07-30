69dadd0256334c0d24a7ba9214d332f5456ac26e
Terrence  Malone <terrenceam22@Terrences-MBP.hsd1.ga.comcast.net>
2020-07-30 15:24:57 -0400

    deploying to heruko

---
+++ APP/visual.py
@@ -0,0 +1,10 @@
+import numpy as numpy
+import matplotlib.pyplot as plt 
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+
+
